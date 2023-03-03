649c4e06b2e0850d9f8860563684a979d0ee161f
Purvi Thakkar <purvi@s2ranalytics.com>
2023-03-03 11:46:20 +0000

    updatin files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-# from data import *
+import pyodbc
+
 
 app = Flask(__name__)
 
@@ -20,7 +21,46 @@ def contactus():
 
 @app.route("/top20")
 def top20():
-    return render_template("top20.html")
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                          'SERVER=byltinsyte-dev-sql-server-eu.database.windows.net;'
+                          'DATABASE=Monitor;'
+                          'UID=byltinsytesqladmin;'
+                          'PWD=bf534015-fa65-4d38-a438-42548e4536cc;')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT TOP(21) id, job_id, event_trigger, event_description, trigger_time FROM dbo.logs2 ORDER BY trigger_time DESC').fetchone()
+    row = cursor.fetchall()
+    return render_template(f'top20.html', top20=row)
+
+@app.route("/top50")
+def top50():
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                          'SERVER=byltinsyte-dev-sql-server-eu.database.windows.net;'
+                          'DATABASE=Monitor;'
+                          'UID=byltinsytesqladmin;'
+                          'PWD=bf534015-fa65-4d38-a438-42548e4536cc;')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT TOP(50) id, job_id, event_trigger, event_description, trigger_time FROM dbo.logs2 ORDER BY trigger_time DESC').fetchone()
+    row = cursor.fetchall()
+    return render_template(f'top50.html', top50=row)
+
+
+
+@app.route("/top100")
+def top100():
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                          'SERVER=byltinsyte-dev-sql-server-eu.database.windows.net;'
+                          'DATABASE=Monitor;'
+                          'UID=byltinsytesqladmin;'
+                          'PWD=bf534015-fa65-4d38-a438-42548e4536cc;')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT TOP(100) id, job_id, event_trigger, event_description, trigger_time FROM dbo.logs2 ORDER BY trigger_time DESC').fetchone()
+    row = cursor.fetchall()
+    return render_template(f'top100.html', top100=row)
+
+app.run()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- data.py
+++ data.py
@@ -1,39 +1,72 @@
 import pyodbc
-import os
-
-print(os.environ['PWD'])
 
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
                       'SERVER=byltinsyte-dev-sql-server-eu.database.windows.net;'
                       'DATABASE=Monitor;'
                       'UID=byltinsytesqladmin;'
                       'PWD=bf534015-fa65-4d38-a438-42548e4536cc;')
-
 cursor = conn.cursor()
 
 print("\n----------------------- DESC -----------------------")
-cursor.execute('select * from dbo.logs ORDER BY trigger_time DESC').fetchone()
+cursor.execute('select * from dbo.logs2 ORDER BY trigger_time DESC').fetchone()
 row = cursor.fetchone()
 print(f"Latest Queue = {row}")
 
 
 print("\n----------------------- Datetime = 20 Queue -----------------------")
-cursor.execute('SELECT TOP(21) id, trigger_time FROM dbo.logs ORDER BY trigger_time DESC').fetchone()
+cursor.execute('SELECT TOP(21) id, job_id, event_trigger, event_description, trigger_time FROM dbo.logs2 ORDER BY trigger_time DESC').fetchone()
 row = cursor.fetchall()
-count = 1
+count = 0
 for i in row:
-    print(f"Queue {count} = {i}")
     count += 1
+    print(f"Queue {count} = {i}")
 
 
-print("\n----------------------- Time = 20 Queue -----------------------")
-cursor.execute('SELECT TOP(21) [trigger_time] FROM dbo.logs ORDER BY trigger_time DESC').fetchone()
+print("\n----------------------- Time = 100 Queue -----------------------")
+cursor.execute('SELECT TOP(101) [trigger_time] FROM dbo.logs2 ORDER BY trigger_time DESC').fetchone()
 row = cursor.fetchall()
 index = 0
 for i in row:
     index += 1
     for j in i:
         print(f"Queue {index} = {j}")
+
+print("\n----------------------- APP -----------------------")
+
+def top20():
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                          'SERVER=byltinsyte-dev-sql-server-eu.database.windows.net;'
+                          'DATABASE=Monitor;'
+                          'UID=byltinsytesqladmin;'
+                          'PWD=bf534015-fa65-4d38-a438-42548e4536cc;')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT TOP(21) id, event_trigger, event_description, trigger_time FROM dbo.logs2 ORDER BY trigger_time DESC').fetchone()
+    result = cursor.fetchall()
+    count = 0
+    for i in result:
+        count += 1
+        print(f"Queue {count} = {i}")
+top20()
+
+
+def top100():
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                          'SERVER=byltinsyte-dev-sql-server-eu.database.windows.net;'
+                          'DATABASE=Monitor;'
+                          'UID=byltinsytesqladmin;'
+                          'PWD=bf534015-fa65-4d38-a438-42548e4536cc;')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT TOP(100) id, job_id, event_trigger, event_description, trigger_time FROM dbo.logs2 ORDER BY trigger_time DESC').fetchone()
+    result = cursor.fetchall()
+    count = 0
+    for i in result:
+        count += 1
+        print(f"Queue {count} = {i}")
+    # return render_template(f'top100.html', top100=result100)
+
+
+top100()
 conn.close()
 
-# SELECT TOP(20) [trigger_time] FROM dbo.logs ORDER BY trigger_time DESC
